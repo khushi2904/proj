a201ed22ff4a73d43958b7742989d46ca64b80f4
khushi2904 <khushdesai2904@gmail.com>
2016-09-16 11:29:01 +0530

    Template for caching

---
+++ caching/caching/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace caching.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ caching/caching/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(caching.Startup))]
+namespace caching
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
